adf6d9169c909251c966504845984c034803badc
scootdboot <andersonscott100@gmail.com>
2023-07-13 14:08:13 -0400

    complete
    things should be completed apart from bug squashing

--- src/Locker.java
+++ src/Locker.java
@@ -17,25 +17,43 @@ public class Locker
     {
         this.number = number;
 
-        this.comboA[0] = random.nextInt(50);
-        this.comboA[1] = random.nextInt(10);
-        this.comboA[2] = random.nextInt(50);
+        comboA[0] = random.nextInt(50);
+        comboA[1] = random.nextInt(10);
+        comboA[2] = random.nextInt(50);
 
-        this.comboB[0] = random.nextInt(50);
-        this.comboB[1] = random.nextInt(10) + 10;
-        this.comboB[2] = random.nextInt(50);
+        comboB[0] = random.nextInt(50);
+        comboB[1] = random.nextInt(10) + 10;
+        comboB[2] = random.nextInt(50);
 
-        this.comboC[0] = random.nextInt(50);
-        this.comboC[1] = random.nextInt(10) + 20;
-        this.comboC[2] = random.nextInt(50);
+        comboC[0] = random.nextInt(50);
+        comboC[1] = random.nextInt(10) + 20;
+        comboC[2] = random.nextInt(50);
 
-        this.comboD[0] = random.nextInt(50);
-        this.comboD[1] = random.nextInt(10) + 30;
-        this.comboD[2] = random.nextInt(50);
+        comboD[0] = random.nextInt(50);
+        comboD[1] = random.nextInt(10) + 30;
+        comboD[2] = random.nextInt(50);
 
-        this.comboE[0] = random.nextInt(50);
-        this.comboE[1] = random.nextInt(10) + 40;
-        this.comboE[2] = random.nextInt(50);
+        comboE[0] = random.nextInt(50);
+        comboE[1] = random.nextInt(10) + 40;
+        comboE[2] = random.nextInt(50);
+    }
+
+    public String toString()
+    {
+        String output = "";
+        output += "Locker: " + number + "\n";
+        output += "A: " + comboA[0] + "-" + comboA[1] + "-" + comboA[2] + "\n";
+        output += "B: " + comboB[0] + "-" + comboB[1] + "-" + comboB[2] + "\n";
+        output += "C: " + comboC[0] + "-" + comboC[1] + "-" + comboC[2] + "\n";
+        output += "D: " + comboD[0] + "-" + comboD[1] + "-" + comboD[2] + "\n";
+        output += "E: " + comboE[0] + "-" + comboE[1] + "-" + comboE[2] + "\n";
+        output += "Contents: ";
+        for (String item : contents)
+        {
+            output += item + ", ";
+        }
+        output = output.substring(0, output.length() - 3);
+        return output;
     }
 
     public String getCombination()
@@ -47,6 +65,40 @@ public class Locker
             case (2) -> { return comboC[0] + "-" + comboC[1] + "-" + comboC[2]; }
             case (3) -> { return comboD[0] + "-" + comboD[1] + "-" + comboD[2]; }
             case (4) -> { return comboE[0] + "-" + comboE[1] + "-" + comboE[2]; }
+        }
+        return "what in the everloving-";
+    }
+
+    public void nextCombination()
+    {
+        comboIndex++;
+    }
+
+    public void addItem(String itemToAdd)
+    {
+        contents.add(itemToAdd);
+    }
+
+    public String getContents()
+    {
+        String output = "";
+        for (String item : contents)
+        {
+            output += item + ", ";
+        }
+        output += "\n";
+        return output;
+    }
+
+    public void removeItem(String itemToRemove)
+    {
+        for (int i = 0; i < contents.size(); i++)
+        {
+            if (contents.get(i).equals(itemToRemove))
+            {
+                contents.remove(i);
+                break;
+            }
         }
     }
 }
